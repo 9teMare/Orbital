4d4b10bffc263925ecc907acbf16b0071015795e
9teMare <wangzihao139@126.com>
2021-06-10 14:53:06 +0800

    attempt to merge

--- component/auth/Login.js
+++ component/auth/Login.js
@@ -29,6 +29,7 @@ export class Login extends Component {
     render() {
         return (
             <View>
+<<<<<<< Updated upstream
 
                 <Text 
                     style ={{fontSize: 30, fontWeight: 'bold', marginLeft: 10, marginTop: 80, alignSelf: 'center'}}>
@@ -36,6 +37,9 @@ export class Login extends Component {
                 </Text>
 
             <View style={{marginTop: '20%'}}>
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
                 <TextInput
                     style={styles.input}
                     placeholder="email"
@@ -50,12 +54,46 @@ export class Login extends Component {
 
                 <TouchableOpacity
                     onPress={ () => this.onSignUp()}
+<<<<<<< Updated upstream
                     style = {{width: 340, height: 50, alignItems:'center', backgroundColor: "black", alignSelf:'center',
                     marginTop: 20}}>
                         <Text style={{color: "white", marginTop: 15}}> SIGN IN </Text>
 
                 </TouchableOpacity>
             </View>
+=======
+                    title="Sign In"
+                />
+=======
+
+                <Text 
+                    style ={{fontSize: 30, fontWeight: 'bold', marginLeft: 10, marginTop: 80, alignSelf: 'center'}}>
+                    GREETING, SUMMONERS!
+                </Text>
+
+                <View style={{marginTop: '20%'}}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="email"
+                        onChangeText={(email) => this.setState({ email })}
+                    />
+                    <TextInput
+                        style = {styles.input}
+                        placeholder="password"
+                        secureTextEntry={true}
+                        onChangeText={(password) => this.setState({ password })}
+                    />
+
+                    <TouchableOpacity
+                        onPress={ () => this.onSignUp()}
+                        style = {{width: 340, height: 50, alignItems:'center', backgroundColor: "black", alignSelf:'center',
+                        marginTop: 20}}>
+                            <Text style={{color: "white", marginTop: 15}}> SIGN IN </Text>
+
+                    </TouchableOpacity>
+                </View>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
             </View>
         )
     }
--- component/main/MyPage.js
+++ component/main/MyPage.js
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import {StyleSheet, View, Text, Button, Image, TouchableOpacity} from 'react-native'
+=======
+import React, { useState } from 'react'
+import {StyleSheet, View, Text, Button, Image, TouchableOpacity, TextInput} from 'react-native'
+>>>>>>> Stashed changes
 import {connect} from 'react-redux'
 import firebase from 'firebase'
 
@@ -12,6 +17,48 @@ function MyPage(props) {
         firebase.auth().signOut()
     }
 
+<<<<<<< Updated upstream
+=======
+    const fetchApiCall = () => {
+        fetch("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-a47cf3c8-1834-402c-a790-dd9e065b2eae", {
+          "method": "GET",
+          "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.41",
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ja;q=0.5,zh-TW;q=0.4",
+            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": "https://developer.riotgames.com"
+          }
+        })
+          .then(response => response.json())
+          .then(response => {
+            console.log(response.name)
+            console.log(response.summonerLevel)
+            console.log(response.profileIconId)
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+
+    //   const axiosApiCall = () => {
+    //     axios({
+    //       "method": "POST",
+    //       "url": "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/9teMare",
+    //       "headers": {
+    //         "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ja;q=0.5,zh-TW;q=0.4",
+    //         "X-Riot-Token": "RGAPI-a47cf3c8-1834-402c-a790-dd9e065b2eae"
+    //       }
+    //     })
+    //       .then((response) => {
+    //         console.log(response.data.content);
+    //         console.log(response.data.originator.name)
+    //       })
+    //       .catch((error) => {
+    //         console.log(error)
+    //       })
+    //   }
+
+>>>>>>> Stashed changes
     return (
         <View>
 
@@ -41,6 +88,7 @@ function MyPage(props) {
             </TouchableOpacity>
 
 
+<<<<<<< Updated upstream
 
 
         <View style={styles.container}>
@@ -49,6 +97,25 @@ function MyPage(props) {
                 onPress={() => onLogout()}
             />
         </View>
+=======
+            <View style={styles.container}>
+                <View style={styles.containerInfo}>
+                    <Text> {currentUser.name} </Text>
+                    <Text> {currentUser.email} </Text>
+                </View>
+                <Button
+                    title="Logout"
+                    onPress={() => onLogout()}
+                />
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={isDarkTheme ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isDarkTheme}
+                />
+            </View>
+>>>>>>> Stashed changes
         </View>
     )
 }
